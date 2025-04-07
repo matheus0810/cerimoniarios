df308ab2a3fac1db19f6718c8790bb4815ac4d44
matheus0810 <matheusgabriel934@gmail.com>
2025-04-07 15:55:10 -0300

    builds888

--- app_cerimoniarios/lib/main.dart
+++ app_cerimoniarios/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  await initializeDateFormatting('pt_BR', null);
+
   print('✅ Firebase inicializado - carregando LoginPage');
 
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: LoginPage(),
   ));
 }
